1711b063aad28344b952373f2fbe90b5dce8f159
Rahul Gupta <guptarahul70322@gmail.com>
2021-06-23 02:51:54 +0530

    model complete

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const programmingLanguagesRouter = require('./routes/programmingLanguages');
-
+const db = require('./services/db')
+const programmingLanguagesModel = require('./models/programmingLanguages')
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -18,7 +19,9 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({'message': err.message});
   return;
 });
-app.listen(process.env.PORT, () => 
+app.listen(process.env.PORT, async () => 
 {
   console.log('Listening on: ' + process.env.PORT)
+  // const result = await db.query(programmingLanguagesModel.deleteRowsQuery)
+  // console.log(result)
 })
--- src/services/db.js
+++ src/services/db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2/promise')
 const dbConfig = require('../config/dbConfig')
 let connection
-(async () => {
-  connection = await mysql.createConnection(dbConfig)
-})()
 async function query(sql, params) {
+  if (!connection) {
+    connection = await mysql.createConnection(dbConfig)
+  }
   const [results] = await connection.execute(sql, params)
   return results;
 }
 module.exports = {
-  query
+  query 
 }
